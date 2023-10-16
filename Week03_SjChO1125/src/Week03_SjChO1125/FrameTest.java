1b977bc58d88b84fb9826f28ea57cfb0576a58f4
SjChO1125 <chosj8575@gmail.com>
2023-10-16 17:21:36 +0900

    5주차

---
+++ Week03_SjChO1125/src/Week03_SjChO1125/FrameTest.java
@@ -0,0 +1,17 @@
+package Week03_SjChO1125;
+import javax.swing.*;
+
+public class FrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f=new JFrame("FrameTest");
+		f.setTitle("MyFrame");
+		f.setSize(300,200);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+	}
+
+}
